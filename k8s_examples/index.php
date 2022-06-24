72503381abe86220de1e65c89e1399463f0e2037
Saimon <farcry2000@mail.ru>
2022-06-24 13:01:16 +0700

    First examples

---
+++ k8s_examples/index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello Valera na kubere</h1><br>";
+echo "Server ip adress is: $ip_server", "<br><p>";
+echo "Mabe by <font color=blue>Dima 2k mmr";
+
+?>
